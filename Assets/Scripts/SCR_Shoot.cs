ac15587f6cf9ef772c57a6cdced0b612192d25f1
Malindu Weerasooriya <f%B8K8X9O&D6QaeT>
2023-07-08 21:09:24 +0530

    Multiple bullet cycling issue fix

--- Assets/Scripts/SCR_Shoot.cs
+++ Assets/Scripts/SCR_Shoot.cs
@@ -13,84 +13,45 @@ public class SCR_Shoot : MonoBehaviour
     public float destroyTime;
     public float bulletSpeed;
 
-    int maxCycles = 10;
-    int currentCycle = 1;
     private void Update()
     {
         counter += Time.deltaTime;
 
-        if (currentCycle < maxCycles && counter >= bpmInterval)
+        if (counter >= bpmInterval)
         {
-            StartCoroutine(Shoot());
+
+            foreach (float shot in currentGun.Bullets)
+            {
+                StartCoroutine(Shoot(shot));
+            }
             counter = 0f;
-            currentCycle++;
         }
     }
 
-    public IEnumerator Shoot()
+    public IEnumerator Shoot(float shot)
     {
         int numberOfShots = currentGun.Bullets.Length;
 
-        foreach (float shot in currentGun.Bullets)
+        GameObject _bullet = BulletPool.SharedInstance.GetPooledObject();
+        if (_bullet != null)
         {
-
-            GameObject _bullet = BulletPool.SharedInstance.GetPooledObject();
-            if (_bullet != null)
-            {
-                _bullet.transform.position = transform.position;
-                _bullet.transform.rotation = player.rotation;
-                _bullet.transform.Rotate(0, shot, 0);
-                _bullet.SetActive(true);
-            }
-
-            Rigidbody rb = _bullet.GetComponent<Rigidbody>();
-
-            rb.velocity = Vector3.zero;
-
-            rb.AddForce(_bullet.transform.forward * bulletSpeed);
-
-            _bullet.transform.parent = null;
-
-            yield return new WaitForSeconds(bpmInterval);
-
-            _bullet.SetActive(false);
+            _bullet.transform.position = transform.position;
+            _bullet.transform.rotation = player.rotation;
+            _bullet.transform.Rotate(0, shot, 0);
+            _bullet.SetActive(true);
         }
 
+        Rigidbody rb = _bullet.GetComponent<Rigidbody>();
 
-        yield return null;
+        rb.velocity = Vector3.zero;
 
+        rb.AddForce(_bullet.transform.forward * bulletSpeed);
+
+        _bullet.transform.parent = null;
+
+
+        yield return new WaitForSeconds(bpmInterval);
+        _bullet.SetActive(false);
 
     }
 }
-
-
-//private IEnumerator ShootSpread()
-//{
-//    IsShooting = true;
-
-//    float angleIncrement = degree / (bulletAmount - 1);
-//    Quaternion startRotation = firePoint.rotation;
-
-//    for (int i = 0; i < bulletAmount; i++)
-//    {
-//        Quaternion spreadRotation = Quaternion.AngleAxis(-degree / 2 + (i * angleIncrement), Vector3.forward);
-//        Quaternion bulletRotation = startRotation * spreadRotation;
-
-//        GameObject projectileObject = Instantiate(projectilePrefab, firePoint.position, bulletRotation);
-
-//        if (projectileObject.TryGetComponent(out ProjectileMovement projectile))
-//        {
-//            Vector3 initialDirection = (Player.position - firePoint.position).normalized;
-//            Vector3 spreadDirection = spreadRotation * initialDirection;
-//            projectile.SetVelocity(spreadDirection, bulletSpeed);
-//            projectile.SetRange(BulletRange);
-//            projectile.SetDamage(damage);
-//            projectile.SetEnemyBullet();
-//            projectile.SetBulletScale(bulletScale);
-//        }
-//    }
-
-//    yield return new WaitForSeconds(RestTime);
-
-//    IsShooting = false;
-//}
