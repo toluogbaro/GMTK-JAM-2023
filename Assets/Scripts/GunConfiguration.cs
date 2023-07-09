3a64aa975481dfc8e99bade4bdb97bef8a02c673
Tolu Ogbaro <ao231197@falmouth.ac.uk>
2023-07-09 10:13:59 +0100

    weapon switching + messed with gun scripts

--- Assets/Scripts/GunConfiguration.cs
+++ Assets/Scripts/GunConfiguration.cs
@@ -6,7 +6,10 @@ public class GunConfiguration : ScriptableObject
     public float FireRate = 0.25f;
     public float[] Bullets = { 0 };
     public string audioParam = "";
+    public GunType gunType;
 }
+
+ public enum GunType { AKIMBO, PISTOL, SHOTGUN };
 
 //class Bullet
 //{
---
+++ Assets/Scripts/Tolu/SCR_WeaponWheel.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SCR_WeaponWheel : MonoBehaviour
+{
+    [SerializeField] SCR_Shoot shootScript;
+    [SerializeField] List<Vector3> wheelPositions;
+    [SerializeField] List<GameObject> wheelCompartments;
+    [SerializeField] GameObject currentWeapon;
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        MoveCycle();
+
+    }
+
+    //void MoveUIPositions()
+    //{
+    //    if (currentWeapon.GetComponent<SCR_WheelCompartment>().gunConfiguration.gunType == shootScript.currentGun.gunType) return;
+
+    //    wheelCompartments[0].LeanMoveLocal(wheelPositions[0], 0.4f).setEaseInBack();
+    //    wheelCompartments[0].LeanScale(new Vector3(1.2f, 1.2f, 1f), 0.4f);
+
+    //    wheelCompartments[1].LeanMoveLocal(wheelPositions[1], 0.4f).setEaseInBack();
+    //    wheelCompartments[1].LeanScale(new Vector3(1f, 1f, 1f), 0.4f);
+
+    //    wheelCompartments[2].LeanMoveLocal(wheelPositions[2], 0.4f).setEaseInBack();
+    //    wheelCompartments[2].LeanScale(new Vector3(1, 1, 1f), 0.4f);
+    //}
+
+    IEnumerator MoveUIPositions()
+    {
+        wheelCompartments[0].LeanMoveLocal(wheelPositions[0], 0.4f).setEaseInBack();
+        wheelCompartments[0].LeanScale(new Vector3(1.2f, 1.2f, 1f), 0.4f);
+
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        wheelCompartments[1].LeanMoveLocal(wheelPositions[1], 0.4f).setEaseInBack();
+        wheelCompartments[1].LeanScale(new Vector3(1f, 1f, 1f), 0.4f);
+
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        wheelCompartments[2].LeanMoveLocal(wheelPositions[2], 0.4f).setEaseInBack();
+        wheelCompartments[2].LeanScale(new Vector3(1, 1, 1f), 0.4f);
+
+        yield return null;
+    }
+
+    void MoveCycle()
+    {
+        if (currentWeapon.GetComponent<SCR_WheelCompartment>().gunConfiguration.gunType == shootScript.currentGun.gunType) return;
+
+        switch (shootScript.currentGun.gunType)
+        {
+            case GunType.PISTOL:
+                foreach(GameObject obj in wheelCompartments)
+                {
+                    if(obj.GetComponent<SCR_WheelCompartment>().gunConfiguration.gunType == GunType.PISTOL)
+                    {
+                        if (obj.GetComponent<SCR_WheelCompartment>().gunConfiguration.gunType == GunType.AKIMBO)
+                        {
+                            currentWeapon = obj;
+                        }
+                    }
+                }
+                if (wheelCompartments[0] != currentWeapon)
+                {
+                    int indexToSwap = wheelCompartments.IndexOf(currentWeapon);
+                    wheelCompartments[3] = wheelCompartments[0];
+                    wheelCompartments[0] = wheelCompartments[indexToSwap];
+                    wheelCompartments[indexToSwap] = wheelCompartments[3];
+
+                }
+                StartCoroutine(MoveUIPositions());
+                break;
+
+            case GunType.AKIMBO:
+                foreach (GameObject obj in wheelCompartments)
+                {
+                    if (obj.GetComponent<SCR_WheelCompartment>().gunConfiguration.gunType == GunType.AKIMBO)
+                    {
+                        currentWeapon = obj;
+                    }
+                }
+                if (wheelCompartments[0] != currentWeapon)
+                {
+                    int indexToSwap = wheelCompartments.IndexOf(currentWeapon);
+                    wheelCompartments[3] = wheelCompartments[0];
+                    wheelCompartments[0] = wheelCompartments[indexToSwap];
+                    wheelCompartments[indexToSwap] = wheelCompartments[3];
+
+                }
+                StartCoroutine(MoveUIPositions());
+                break;
+
+            case GunType.SHOTGUN:
+                foreach (GameObject obj in wheelCompartments)
+                {
+                    if (obj.GetComponent<SCR_WheelCompartment>().gunConfiguration.gunType == GunType.SHOTGUN)
+                    {
+                        currentWeapon = obj;
+                    }
+                }
+                if (wheelCompartments[0] != currentWeapon)
+                {
+                    int indexToSwap = wheelCompartments.IndexOf(currentWeapon);
+                    wheelCompartments[3] = wheelCompartments[0];
+                    wheelCompartments[0] = wheelCompartments[indexToSwap];
+                    wheelCompartments[indexToSwap] = wheelCompartments[3];
+
+                }
+                StartCoroutine(MoveUIPositions());
+                break;
+
+        }
+    }
+
+
+
+
+}
